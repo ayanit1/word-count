1e58cb186097aacce45b68c02a40a61829588304
Albert <ayanit1@googlemail.com>
2017-06-12 00:18:41 +0100

    refactor

--- spec/counterSpec.js
+++ spec/counterSpec.js
@@ -1,15 +1,17 @@
 describe("Counter", function(){
   var Counter = require('../lib/counter')
 
+  beforeEach(function(){
+    counter = new Counter
+  });
+
   describe("when created", function(){
 
     it("has an empty wordArray", function(){
-      counter = new Counter();
       expect(counter.wordArray).toEqual([]);
     })
 
     it("has an empty wordOccurences hash", function(){
-      counter = new Counter();
       expect(counter.wordOccurences).toEqual({});
     })
   });

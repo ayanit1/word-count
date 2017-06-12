4f5d1e3080049e500ead8b2fd1ff61fb7c1301ee
Albert <ayanit1@googlemail.com>
2017-06-12 01:12:00 +0100

    refactor

--- spec/counterSpec.js
+++ spec/counterSpec.js
@@ -14,14 +14,17 @@ describe("Counter", function(){
     it("has an empty wordOccurences hash", function(){
       expect(counter.wordOccurences).toEqual({});
     })
+
   });
 
-  describe("#addWordArray", function(){
+  describe("#addToWordArray", function(){
     var data = "this test"
 
     it("separates a sentence and adds to wordArray", function(){
-      counter.addWordArray(data);
+      counter.addToWordArray(data);
       expect(counter.wordArray).toEqual(["this", "test"])
     })
+
   });
+
 });
